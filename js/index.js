b95cad5f068cfd456b2debabc5c26ee4cd9532db
abarabonova <55283014+abarabonova@users.noreply.github.com>
2020-05-22 00:01:25 +0300

    base

--- js/index.js
+++ js/index.js
@@ -2,3 +2,9 @@
 [...document.querySelectorAll('.fluid-text')].forEach((item) => {
   fitText(item, 0.305)
 });
+
+// статистика
+//text
+[...document.querySelectorAll('.statistics_heading')].forEach((item) => {
+  fitText(item, 0.443)
+});
