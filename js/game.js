83ddd9c8ecaecac219ca2048b5635cd9ed704abc
abarabonova <55283014+abarabonova@users.noreply.github.com>
2020-05-25 17:43:08 +0300

    поиск

---
+++ js/game.js
@@ -0,0 +1,21 @@
+let counter = document.getElementsByClassName('.game__time__number')
+let shapeYellowOval = document.getElementsByClassName('.game__shapes__oval-yellow')
+
+counter.value = 0;
+
+shapeYellowOval.onclick = function () {
+  counter.value =+ 1
+
+}
+
+// 2
+// document.addEventListener('click', function (e) {
+//   if (e.target.classList.contains("counter")) {
+//     ++e.target.parentElement.querySelector(".shapeYellowOval").value;
+//   }
+// })
+
+
+if (count == "") {
+  document.write = 0
+}
