55ac267e687409af63bfa1527b2f25deaeec36ec
abarabonova <55283014+abarabonova@users.noreply.github.com>
2020-05-30 00:04:45 +0300

    промокод + картинка у ивентов

--- js/sale.js
+++ js/sale.js
@@ -1,10 +1,19 @@
 // ellipse-red
-let ellipseRed = anime({
-  targets: '.ellipse-red',
+// let ellipseRed = anime({
+//   targets: '.ellipse-red',
+//   easing: 'linear',
+//   translateX: [0,200],
+//   translateY: [0,200],
+//   duration: 25000,
+//   direction: 'alternate',
+//   loop: true
+// })
+
+let saleSale = anime({
+  targets: '.sale',
   easing: 'linear',
-  translateX: [0,200],
-  translateY: [0,200],
-  duration: 25000,
+  translateX: [0,1000],
+  duration: 4000,
   direction: 'alternate',
   loop: true
 })
