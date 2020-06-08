556fc8cc0d666b75db3ebff76e3a49d1e3a6f144
abarabonova <55283014+abarabonova@users.noreply.github.com>
2020-06-08 16:24:31 +0300

    game OVER

--- js/game.js
+++ js/game.js
@@ -9,36 +9,169 @@ let shape7 = document.getElementById('shape-7')
 let int
 
 
-window.onload = function changeColorOne() {
+document.addEventListener("DOMContentLoaded", __main__)
 
-  int = setInterval(function(){
-  shape1.style.backgroundColor = '#70348C'
-  setTimeout(function () {
-     shape1.style.backgroundColor = '#00844B'
-  }, 800)
-}, 3600)
+function __main__() {
+  let shape1 = document.getElementById('shape-1')
+  let interval
+
+  startColorChange()
+  setClickCounter()
+
+  function startColorChange() {
+    interval = setInterval(function() {
+      shape1.classList.add("active")
+
+      setTimeout(function() {
+        shape1.classList.remove("active")
+      }, 800)
+    }, 3600)
+
+    interval = setInterval(function() {
+      shape2.classList.add("active")
+
+      setTimeout(function() {
+        shape2.classList.remove("active")
+      }, 1000)
+    }, 6000)
+
+    interval = setInterval(function() {
+      shape3.classList.add("active")
+
+      setTimeout(function() {
+        shape3.classList.remove("active")
+      }, 500)
+    }, 7000)
+
+    interval = setInterval(function() {
+      shape4.classList.add("active")
+
+      setTimeout(function() {
+        shape4.classList.remove("active")
+      }, 500)
+    }, 17000)
+
+    interval = setInterval(function() {
+      shape5.classList.add("active")
+
+      setTimeout(function() {
+        shape5.classList.remove("active")
+      }, 700)
+    }, 11400)
+
+    interval = setInterval(function() {
+      shape6.classList.add("active")
+
+      setTimeout(function() {
+        shape6.classList.remove("active")
+      }, 300)
+    }, 8130)
+
+    interval = setInterval(function() {
+      shape7.classList.add("active")
+
+      setTimeout(function() {
+        shape7.classList.remove("active")
+      }, 500)
+    }, 5620)
+  }
+
+  function setClickCounter() {
+    let count = 0
+    let span = document.getElementById('game__time')
+    span.textContent = count
+
+    document.addEventListener('click', function(e) {
+      let shape1 = e.target.closest("#shape-1")
+      if (!shape1) return
+
+      if (shape1.classList.contains("active")) {
+        span.textContent = ++count
+      } else {
+        console.log("Промах!")
+        setTimeout(e => console.clear(), 1000)
+      }
+    })
+
+    document.addEventListener('click', function(e) {
+      let shape2 = e.target.closest("#shape-2")
+      if (!shape2) return
+
+      if (shape2.classList.contains("active")) {
+        span.textContent = ++count
+      } else {
+        console.log("Промах!")
+        setTimeout(e => console.clear(), 1000)
+      }
+    })
+
+    document.addEventListener('click', function(e) {
+      let shape3 = e.target.closest("#shape-3")
+      if (!shape3) return
+
+      if (shape3.classList.contains("active")) {
+        span.textContent = ++count
+      } else {
+        console.log("Промах!")
+        setTimeout(e => console.clear(), 1000)
+      }
+    })
+
+    document.addEventListener('click', function(e) {
+      let shape4 = e.target.closest("#shape-4")
+      if (!shape4) return
+
+      if (shape4.classList.contains("active")) {
+        span.textContent = ++count
+      } else {
+        console.log("Промах!")
+        setTimeout(e => console.clear(), 1000)
+      }
+    })
+
+    document.addEventListener('click', function(e) {
+      let shape5 = e.target.closest("#shape-5")
+      if (!shape5) return
+
+      if (shape5.classList.contains("active")) {
+        span.textContent = ++count
+      } else {
+        console.log("Промах!")
+        setTimeout(e => console.clear(), 1000)
+      }
+    })
+
+    document.addEventListener('click', function(e) {
+      let shape6 = e.target.closest("#shape-6")
+      if (!shape6) return
+
+      if (shape6.classList.contains("active")) {
+        span.textContent = ++count
+      } else {
+        console.log("Промах!")
+        setTimeout(e => console.clear(), 1000)
+      }
+    })
+
+    document.addEventListener('click', function(e) {
+      let shape7 = e.target.closest("#shape-7")
+      if (!shape7) return
+
+      if (shape7.classList.contains("active")) {
+        span.textContent = ++count
+      } else {
+        console.log("Промах!")
+        setTimeout(e => console.clear(), 1000)
+      }
+    })
+
+
+
+
+
+
+  }
 }
-
-
-// window.onload = function () {
-	span = document.getElementById('game__time')
-	let count = 0
-	span.innerHTML = count
-
-	document.body.onclick = function (e) {
-		e = e || event
-		let target = e.target || e.srcElement
-		if (target.id === 'shape-1' || 'shape-2' || 'shape-3' || 'shape-4' || 'shape-5' || 'shape-6' || 'shape-7') {
-			count++
-			span.innerHTML = count
-		}
-	}
-// }
-
-
-
-
-
 
 
 
