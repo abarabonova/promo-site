9110d96c7e9254cc15803e2417c9cbf285b4304e
abarabonova <55283014+abarabonova@users.noreply.github.com>
2020-06-12 00:38:16 +0300

    sale + events

--- js/events-ticker.js
+++ js/events-ticker.js
@@ -1,39 +1,3 @@
-//
-//
-//
-//
-//
-// let svgMotion = anime({
-//   targets: '.events__div__span',
-//   easing: 'linear',
-//   translateX: [0,-5000],
-//   duration: 20000,
-//   direction: 'alternate',
-//   loop: true
-// })
-//
-//
-// let svgMotion__2 = anime({
-//   targets: '#events__div__span__2',
-//   easing: 'linear',
-//   translateX: [0,-5000],
-//   duration: 15000,
-//   direction: 'alternate',
-//   loop: true
-// })
-//
-// let svgMotion__3 = anime({
-//   targets: '#events__div__span__3',
-//   easing: 'linear',
-//   translateX: [0,-2000],
-//   duration: 25000,
-//   direction: 'alternate',
-//   loop: true
-// })
-//
-//
-//
-//
 // // statistics
 //
 let motionStatistics = anime({
---
+++ js/events.js
@@ -0,0 +1,160 @@
+window.onload = function () {
+  let posterOne = document.querySelector('#poster-black');
+  posterOne.onmouseout = function(e) {
+    document.getElementById('poster-1').style.display='none';
+  }
+  posterOne.onmouseover = function(e) {
+    document.getElementById('poster-1').style.display='block';
+  }
+
+
+  let posterOneOne = document.querySelector('#poster-black-1');
+  posterOneOne.onmouseout = function(e) {
+    document.getElementById('poster-1').style.display='none';
+  }
+  posterOneOne.onmouseover = function(e) {
+    document.getElementById('poster-1').style.display='block';
+  }
+
+
+
+  let posterOneOneOne = document.querySelector('#poster-black-1-1');
+  posterOneOneOne.onmouseout = function(e) {
+    document.getElementById('poster-1').style.display='none';
+  }
+  posterOneOneOne.onmouseover = function(e) {
+    document.getElementById('poster-1').style.display='block';
+  }
+
+
+
+  let posterTwo = document.querySelector('#poster-black-two');
+  posterTwo.onmouseout = function(e) {
+    document.getElementById('poster-2').style.display='none';
+  }
+  posterTwo.onmouseover = function(e) {
+    document.getElementById('poster-2').style.display='block';
+  }
+
+
+
+  let posterTwoTwo = document.querySelector('#poster-black-two-2');
+  posterTwoTwo.onmouseout = function(e) {
+    document.getElementById('poster-2').style.display='none';
+  }
+  posterTwoTwo.onmouseover = function(e) {
+    document.getElementById('poster-2').style.display='block';
+  }
+
+
+  let posterThree = document.querySelector('#poster-black-three');
+  posterThree.onmouseout = function(e) {
+    document.getElementById('poster-3').style.display='none';
+  }
+  posterThree.onmouseover = function(e) {
+    document.getElementById('poster-3').style.display='block';
+  }
+
+
+  let posterThreeThreeThree = document.querySelector('#poster-black-three-3-3');
+  posterThreeThreeThree.onmouseout = function(e) {
+    document.getElementById('poster-3').style.display='none';
+  }
+  posterThreeThreeThree.onmouseover = function(e) {
+    document.getElementById('poster-3').style.display='block';
+  }
+
+
+
+  let posterThreeThree = document.querySelector('#poster-black-three-3');
+  posterThreeThree.onmouseout = function(e) {
+    document.getElementById('poster-3').style.display='none';
+  }
+  posterThreeThree.onmouseover = function(e) {
+    document.getElementById('poster-3').style.display='block';
+  }
+
+
+  let posterSix = document.querySelector('#poster-red-6');
+  posterSix.onmouseout = function(e) {
+    document.getElementById('poster-6').style.display='none';
+  }
+  posterSix.onmouseover = function(e) {
+    document.getElementById('poster-6').style.display='block';
+  }
+
+
+
+  let posterDjFour = document.querySelector('#poster-dj');
+  posterDjFour.onmouseout = function(e) {
+    document.getElementById('poster-4').style.display='none';
+  }
+  posterDjFour.onmouseover = function(e) {
+    document.getElementById('poster-4').style.display='block';
+  }
+
+
+  let posterDjFour1 = document.querySelector('#poster-dj-1');
+  posterDjFour1.onmouseout = function(e) {
+    document.getElementById('poster-4').style.display='none';
+  }
+  posterDjFour1.onmouseover = function(e) {
+    document.getElementById('poster-4').style.display='block';
+  }
+
+
+  let posterDjFour2 = document.querySelector('#poster-dj-2');
+  posterDjFour2.onmouseout = function(e) {
+    document.getElementById('poster-4').style.display='none';
+  }
+  posterDjFour2.onmouseover = function(e) {
+    document.getElementById('poster-4').style.display='block';
+  }
+
+  let posterDjFour3 = document.querySelector('#poster-dj-3');
+  posterDjFour3.onmouseout = function(e) {
+    document.getElementById('poster-4').style.display='none';
+  }
+  posterDjFour3.onmouseover = function(e) {
+    document.getElementById('poster-4').style.display='block';
+  }
+
+  let posterDjFive = document.querySelector('#poster-dj_2');
+  posterDjFive.onmouseout = function(e) {
+    document.getElementById('poster-5').style.display='none';
+  }
+  posterDjFive.onmouseover = function(e) {
+    document.getElementById('poster-5').style.display='block';
+  }
+
+
+  let posterDjFive1 = document.querySelector('#poster-dj_2-1');
+  posterDjFive1.onmouseout = function(e) {
+    document.getElementById('poster-5').style.display='none';
+  }
+  posterDjFive1.onmouseover = function(e) {
+    document.getElementById('poster-5').style.display='block';
+  }
+
+  let posterDjFive2 = document.querySelector('#poster-dj_2-2');
+  posterDjFive2.onmouseout = function(e) {
+    document.getElementById('poster-5').style.display='none';
+  }
+  posterDjFive2.onmouseover = function(e) {
+    document.getElementById('poster-5').style.display='block';
+  }
+
+  let posterDjFive3 = document.querySelector('#poster-dj_2-3');
+  posterDjFive3.onmouseout = function(e) {
+    document.getElementById('poster-5').style.display='none';
+  }
+  posterDjFive3.onmouseover = function(e) {
+    document.getElementById('poster-5').style.display='block';
+  }
+
+
+
+
+
+
+}
